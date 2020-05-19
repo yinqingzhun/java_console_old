1d9a4616a74a9531d21d5b170e78a756f410976c
yinqingzhun <yinqingzhun@autohome.com.cn>
2020-05-19 23:40:24 +0800

    add comment

--- src/main/java/com/yqz/console/lock/FIFOMutex.java
+++ src/main/java/com/yqz/console/lock/FIFOMutex.java
@@ -5,31 +5,34 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.LockSupport;
 
+/**
+ * LockSupport 示例代码
+ * lock将当前线程加入等待队列，一直循环判断自己是否队首的线程且取锁成功。每个线程都做这样的操作，直到自己取锁成功。此时线程将会被从队首中移除
+ */
 public class FIFOMutex {
-   private final AtomicBoolean locked = new AtomicBoolean(false);
-   private final Queue<Thread> waiters
-     = new ConcurrentLinkedQueue<Thread>();
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+    private final Queue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
 
-   public void lock() {
-     boolean wasInterrupted = false;
-     Thread current = Thread.currentThread();
-     waiters.add(current);
+    public void lock() {
+        boolean wasInterrupted = false;
+        Thread current = Thread.currentThread();
+        waiters.add(current);
 
-     // Block while not first in queue or cannot acquire lock
-     while (waiters.peek() != current ||
-            !locked.compareAndSet(false, true)) {
-       LockSupport.park(this);
-       if (Thread.interrupted()) // ignore interrupts while waiting
-         wasInterrupted = true;
-     }
+        // Block while not first in queue or cannot acquire lock
+        while (waiters.peek() != current ||
+                !locked.compareAndSet(false, true)) {
+            LockSupport.park(this);
+            if (Thread.interrupted()) // ignore interrupts while waiting
+                wasInterrupted = true;
+        }
 
-     waiters.remove();
-     if (wasInterrupted)          // reassert interrupt status on exit
-       current.interrupt();
-   }
+        waiters.remove();
+        if (wasInterrupted)          // reassert interrupt status on exit
+            current.interrupt();
+    }
 
-   public void unlock() {
-     locked.set(false);
-     LockSupport.unpark(waiters.peek());
-   }
- }
+    public void unlock() {
+        locked.set(false);
+        LockSupport.unpark(waiters.peek());
+    }
+}
